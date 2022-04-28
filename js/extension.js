d0b5d88d1fe405d1a87d466da3db44f35f35af3f
Arist0v <arist0v@disroot.org>
2022-04-28 13:07:03 -0400

    add extensions details to try to make it work

--- js/extension.js
+++ js/extension.js
@@ -11,6 +11,12 @@
 
         load(){
             this.content = '';
+            return fetch('/extensions/${this.id}/views/content.html')
+            .then((res)=> res.text())
+            .then((text) =>{
+                this.content = text;
+            })
+            .catch((e) => console.error('Failed to fetch content:', e))
         }
 
         show(){
