1a6892b0c6da833e56143f2caaf6c0eb0bcea250
Arist0v <arist0v@disroot.org>
2022-04-29 14:42:55 -0400

    test descriptions

--- js/extension.js
+++ js/extension.js
@@ -32,7 +32,7 @@
 			API.getThings().then((things)=>{
 				
 				for (let key in things){
-					this.test = this.test + things[key]['title'] + '\r\n';
+					this.test = this.test + things[key]['properties'] + '\r\n';
 				}
 				document.getElementById("extension-sinope-out-temp-test").innerHTML = this.test;
 			})
