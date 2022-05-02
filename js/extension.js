290402b3b6dad1ce57e737bfd67a2d775ec68db2
Arist0v <arist0v@disroot.org>
2022-05-01 20:51:25 -0400

    debug

--- js/extension.js
+++ js/extension.js
@@ -33,7 +33,7 @@
 				}
 
 				console.log(this.sinope_thermostats.length);
-				if(this.sinope_thermostats.length < 0){
+				if(this.sinope_thermostats.length < 1){
 					document.getElementById('extension-sinope-out-temp-warning')
 					.innerHTML = 'No sinope Thermostat found. Did you have any on the gateway?'
 					return;
